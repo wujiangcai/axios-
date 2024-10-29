beba5727e513028a556d46dd712e1a9466b6bf17
wujiang_cai <468269530@qq.com>
2024-10-29 17:27:49 +0800

    添加图片功能修改

--- product-management-api/src/main/java/top/wujiangcai/productmanagementapi/controller/ProductController.java
+++ product-management-api/src/main/java/top/wujiangcai/productmanagementapi/controller/ProductController.java
@@ -1,47 +1,43 @@
 package top.wujiangcai.productmanagementapi.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import top.wujiangcai.productmanagementapi.entity.Product;
 import top.wujiangcai.productmanagementapi.service.ProductService;
+import top.wujiangcai.productmanagementapi.utils.OssUtil;
 
 import java.util.List;
 
-/**
- * @author dfysa
- * @data 2024/10/29 上午10:30
- * @description
- */
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
-    private  final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    private final ProductService productService;
+    private final OssUtil ossUtil;
 
     @GetMapping
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productService.list();
-
     }
 
+    // 查询单个商品
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id){
+    public Product getProductById(@PathVariable Long id) {
         return productService.getById(id);
     }
 
+    // 创建商品
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Boolean createProduct(@RequestBody Product product){
+    public Boolean createProduct(@RequestBody Product product) {
         return productService.save(product);
     }
 
-    //  更新商品
+    // 更新商品
     @PutMapping("/{id}")
-    public  Boolean updateProduct(@PathVariable Long id,@RequestBody Product product)
-    {
+    public Boolean updateProduct(@PathVariable Long id, @RequestBody Product product) {
         product.setId(id);
         return productService.updateById(product);
     }
@@ -49,8 +45,13 @@ public class ProductController {
     // 删除商品
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Boolean deleteProduct(@PathVariable Long id){
+    public Boolean deleteProduct(@PathVariable Long id) {
         return productService.removeById(id);
     }
 
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        System.out.println(file.getOriginalFilename());
+        return ossUtil.uploadFile(file);
+    }
 }
